d6e2a0d33d99ee484932a4d218913ab802d13ee5
imbillah <imbilla99@gmail.com>
2023-05-16 10:50:05 -0400

    user follow/unfollow implemented

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -7,6 +7,7 @@ import useUser from "@/hooks/useUser";
 import useEditModal from "@/hooks/useEditModal";
 
 import Button from "../Button";
+import useFollow from "@/hooks/useFollow";
 
 interface UserBioProps {
   userId: string;
@@ -17,7 +18,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   const { data: fetchedUser } = useUser(userId);
 
   const editModal = useEditModal();
-
+  const { isFollowing, toggleFollow } = useFollow(userId);
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) {
       return null;
@@ -32,7 +33,12 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         {currentUser?.id === userId ? (
           <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
-          <Button onClick={() => {}} label={"Follow"} secondary />
+          <Button
+            onClick={toggleFollow}
+            label={isFollowing ? "Unfollow" : "Follow"}
+            secondary={!isFollowing}
+            outline={isFollowing}
+          />
         )}
       </div>
       <div className="mt-8 px-4">
